1d02536470297b3d7dd0a89f91f83af7e090e5a9
Diva gupta <divagupta712@gmail.com>
2024-06-21 04:16:03 +0530

    understood basic file structure

---
+++ 01React/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Diva
+      </h1>
+      <p>I am working on skimzo startup</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01React/src/Diva.jsx
@@ -0,0 +1,7 @@
+function Diva() {
+    return (
+        <h3>I am full stack developer !!</h3>
+    )
+}
+
+export default Diva
